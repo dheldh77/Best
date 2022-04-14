593d9c3b51c076812d68d2aea37de5b8ea5c11af
dh.doh <donghun059@gmail.com>
2022-04-14 16:34:36 +0900

    [Reader] read 함수 생성
    file read 후 List에 담아 return 하도록 생성

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,19 +1,17 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Reader {
-
     //private Option option;
-    public ArrayList<String> read(String filename) {
-        // file read
-        // parsing
-
-        // put data into ArrayList
-
+    public static List<String> read(String filename) {
+        try {
+            return Files.readAllLines(Paths.get(filename));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            return null;
+        }
         // option.setReadOption(...);
-        return null;
     }
-
 }
