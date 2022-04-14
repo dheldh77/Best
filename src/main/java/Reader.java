8557d9b5006e6d3203811621eb5022052ff9f85a
dh.doh <donghun059@gmail.com>
2022-04-14 16:07:31 +0900

    [feature] Reader 클래스 생성

--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,2 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileReader;
+
 public class Reader {
+
+    //private Option option;
+    public ArrayList<String> read(String filename) {
+        // file read
+        // parsing
+
+        // put data into ArrayList
+
+        // option.setReadOption(...);
+        return null;
+    }
 }
