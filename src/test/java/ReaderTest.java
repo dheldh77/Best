f073ba623e5a81c07a49c2a4751a160fefdfb16f
dh.doh <donghun059@gmail.com>
2022-04-15 09:32:08 +0900

    [ReaderTest] input 파일 경로 수정
    프로젝트 경로로 수정하였습니다.

--- src/test/java/ReaderTest.java
+++ src/test/java/ReaderTest.java
@@ -7,7 +7,7 @@ public class ReaderTest {
     @Test
     void readTest(){
         Reader reader = new Reader();
-        List<String > lines = reader.read("C:\\Users\\DHDoh\\Downloads\\input_20_20.txt");
+        List<String > lines = reader.read("../resources/input_20_20.txt");
         assertEquals(lines.size(), 40);
 
         for(String line : lines){
