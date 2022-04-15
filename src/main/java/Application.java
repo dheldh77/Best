7d73911ca4be2e91d5b63f8cb84fda7988dd5830
이재효 <jaehyo91.lee@gmail.com>
2022-04-15 13:26:05 +0900

    method 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Commander cmd = new Commander();
+        cmd.init();
+        cmd.run();
+
     }
 }
--- src/main/java/Commander.java
+++ src/main/java/Commander.java
@@ -1,17 +1,33 @@
+import java.util.ArrayList;
+
 public class Commander {
     Reader reader;
     Executor executor;
     Printer printer;
+    EmployeeService empService;
 
-    public void Read(){
+    public void init() {
+        reader = new Reader();
+        executor = new Executor();
+        printer = new Printer();
+        empService = new EmployeeService();
+    }
+
+    private ArrayList<ArrayList<String>> Read(){
+        return null;
+    }
+
+    private void Execute(){
 
     }
 
-    public void Execute(){
+    private void Print(){
 
     }
 
-    public void Print(){
-
+    public void run() {
+        ArrayList<ArrayList<String>> cmd = Read();
+        Execute();
+        Print();
     }
 }
