6add8fd4f1db3e713fe3c4c969783b7ae14658cf
sh.june <sodlfma1203@gmail.com>
2022-04-15 11:46:45 +0900

    [Name] fullname 추가 및 기능 Update
    Signed-off-by: sh.june <sodlfma1203@gmail.com>

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -1,17 +1,28 @@
 public class Name {
     private String firstName;
     private String SecondName;
+    private String name;
     public Name(String name){
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
         this.firstName = name.split("\\s")[0];
         this.SecondName = name.split("\\s")[1];
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+        this.name = firstName + " " + this.SecondName;
     }
 
     public String getSecondName() {
@@ -19,6 +30,7 @@ public class Name {
     }
 
     public void setSecondName(String secondName) {
-        SecondName = secondName;
+        this.SecondName = secondName;
+        this.name = this.firstName + " " + secondName;
     }
 }
--- src/test/java/NameTest.java
+++ src/test/java/NameTest.java
@@ -14,14 +14,21 @@ class NameTest {
         assertEquals(name.getSecondName(), "JHOP");
     }
     @Test
+    public void name_Getter(){
+        assertEquals(name.getName(), "VXIHXOTH JHOP");
+    }
+
+    @Test
     public void FirstName_Setter(){
         name.setFirstName("ATHKERMD");
         assertEquals(name.getFirstName(),  "ATHKERMD");
+        assertEquals(name.getName(), "ATHKERMD JHOP");
     }
 
     @Test
     public void SecondName_Setter(){
         name.setSecondName("HKTNEKE");
         assertEquals(name.getSecondName(),  "HKTNEKE");
+        assertEquals(name.getName(), "VXIHXOTH HKTNEKE");
     }
 }
