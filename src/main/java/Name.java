5753a646e8b195aa8458a778e301f4b433efef06
sh.june <sodlfma1203@gmail.com>
2022-04-15 11:26:13 +0900

    [Name] Name 클래스 및 Test 코드 구현
    Signed-off-by: sh.june <sodlfma1203@gmail.com>

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -1,2 +1,24 @@
 public class Name {
+    private String firstName;
+    private String SecondName;
+    public Name(String name){
+        this.firstName = name.split("\\s")[0];
+        this.SecondName = name.split("\\s")[1];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return SecondName;
+    }
+
+    public void setSecondName(String secondName) {
+        SecondName = secondName;
+    }
 }
