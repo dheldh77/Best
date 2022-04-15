206ce473032ac8dd74a7aa56594d2df84258bc35
dh.doh <donghun059@gmail.com>
2022-04-15 15:36:25 +0900

    [PrinterTest] PrinterTest 초기 구현

---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class PrinterTest {
+    @Test
+    void printNoneTest() {
+        fail("Not yet implemented");
+    }
+    @Test
+    void printRecordCntTest() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void printRecordTest(){
+        fail("Not yet implemented");
+    }
+}
