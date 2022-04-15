aad3643c4db70b736452fc569425807f0e32ea01
이재효 <jaehyo91.lee@gmail.com>
2022-04-15 16:48:50 +0900

    [Commander,Service] method 추가

--- src/main/java/Commander.java
+++ src/main/java/Commander.java
@@ -1,31 +1,51 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Commander {
-    Reader reader;
     Executor executor;
     Printer printer;
+    EmployeeService empServ;
 
     public void init() {
-        reader = new Reader();
         executor = new Executor();
         printer = new Printer();
+        empServ = new EmployeeService();
     }
 
     private ArrayList<ArrayList<String>> Read(String fileName){
-        return reader.read(fileName);
+        return new Reader().read(fileName);
     }
 
-    private void Execute(){
-
+    private ArrayList<Employee> Execute(ArrayList<String> cmd){
+        switch(cmd.get(0)){
+            case "ADD" :
+//                return executor.add(cmd, empServ);
+                break;
+            case "MOD" :
+//                return executor.modify(cmd, empServ);
+                break;
+            case "DEL" :
+//                return executor.delete(cmd, empServ);
+                break;
+            case "SCH" :
+//                return executor.search(cmd, empServ);
+                break;
+            default :
+//                return null;
+        }
+        return null;
     }
 
-    private void Print(){
-
+    private void Print(InputOption printOption, ArrayList<Employee> employees){
+//        printer.print(printOption, employees);
     }
 
     public void run() {
-        ArrayList<ArrayList<String>> cmd = Read("src\\test\\resources\\input_20_20.txt");
-        Execute();
-        Print();
+        ArrayList<ArrayList<String>> cmds = Read("src\\test\\resources\\input_20_20.txt");
+        for (Iterator<ArrayList<String>> iter = cmds.stream().iterator(); iter.hasNext(); ) {
+            ArrayList<String> cmd = iter.next();
+            ArrayList<Employee> employees = Execute(cmd);
+            Print(InputOption.getInstance(), employees);
+        }
     }
 }
--- src/test/java/CommanderTest.java
+++ src/test/java/CommanderTest.java
@@ -1,20 +1,58 @@
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.*;
+
 public class CommanderTest {
     @Test
     void initTest(){
-
+        Commander commander = mock(Commander.class);
+        commander.init();
+        verify(commander).init();
     }
     @Test
-    void readTest(){
-
+    void readTest() throws Exception {
+        Commander commander = new Commander();
+        Method method = commander.getClass().getDeclaredMethod("Read", String.class);
+        method.setAccessible(true);
+        ArrayList<ArrayList<String>> result = (ArrayList<ArrayList<String>>) method.invoke(commander, "src\\test\\resources\\input_20_20.txt");
+        assertEquals(40, result.size());
     }
     @Test
-    void executeTest(){
-
+    void executeTest() throws Exception {
+        Commander commander = new Commander();
+        EmployeeService empServ = new EmployeeService();
+        ArrayList<String> cmd = new ArrayList<String>();
+        cmd.add("ADD");
+        cmd.add("ADD");
+        cmd.add("ADD");
+        cmd.add("ADD");
+        cmd.add("ADD");
+        commander.init();
+        Method method = commander.getClass().getDeclaredMethod("Execute", ArrayList.class);
+        method.setAccessible(true);
+        ArrayList<Employee> result = (ArrayList<Employee>) method.invoke(commander, cmd);
+        assertNull(result);
     }
     @Test
-    void printTest(){
-
+    void printTest() throws Exception{
+        Commander commander = new Commander();
+        EmployeeService empServ = new EmployeeService();
+        ArrayList<String> cmd = new ArrayList<String>();
+        cmd.add("ADD");
+        cmd.add("ADD");
+        cmd.add("ADD");
+        cmd.add("ADD");
+        cmd.add("ADD");
+        commander.init();
+        Method method = commander.getClass().getDeclaredMethod("Print", InputOption.class, ArrayList.class);
+        method.setAccessible(true);
+        method.invoke(commander, InputOption.getInstance(), cmd);
+//        verify(commander).print();
     }
 }
