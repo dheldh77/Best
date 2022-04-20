b08bcd806bb181cac4343bafcfc3ebcb602c4b53
yujin922 <yujin_920922@naver.com>
2022-04-20 10:29:45 +0900

    [Search 패키지] 접근제어자 변경 및 if문 제거

--- src/main/java/Search/NameSearch.java
+++ src/main/java/Search/NameSearch.java
@@ -5,20 +5,18 @@ import Option.Option;
 import Option.InputOption;
 
 public class NameSearch implements SearchPolicy {
-    Option option;
-    String source;
-    Employee emp;
+    private Option option;
+    private String source;
+    private Employee emp;
 
 
     @Override
     public boolean search(Employee emp, String source)  {
-        this.option = InputOption.getInstance().getActivatedOption(1);
+        this.option = InputOption.getInstance().getActivatedOption(InputOption.getInstance().OPTION2);
         this.source=source;
         this.emp = emp;
-        if(option==Option.NONE) {
-            return matchAll(source);
-        }
-        return matchPart();
+
+        return (option==Option.NONE) ?matchAll(source):matchPart();
     }
 
     private boolean matchAll(String source) {
@@ -26,7 +24,15 @@ public class NameSearch implements SearchPolicy {
     }
 
     private boolean matchPart() {
-        return ((option==Option.FIRST_NAME) && source.equals(emp.getFirstName()))||(option==Option.LAST_NAME && source.equals(emp.getSecondName()));
+        return matchFisrtName()||matchSecondName();
+    }
+
+    private boolean matchFisrtName() {
+        return (option==Option.FIRST_NAME) && source.equals(emp.getFirstName());
+    }
+
+    private boolean matchSecondName(){
+        return (option==Option.LAST_NAME) && source.equals(emp.getSecondName());
     }
 
 }
--- src/main/java/Search/PhoneNumberSearch.java
+++ src/main/java/Search/PhoneNumberSearch.java
@@ -5,20 +5,17 @@ import Option.Option;
 import Option.InputOption;
 
 public class PhoneNumberSearch implements SearchPolicy {
-    Option option;
-    String source;
-    Employee emp ;
+    private Option option;
+    private String source;
+    private Employee emp ;
 
     @Override
     public boolean search(Employee emp, String source) {
-        this.option = InputOption.getInstance().getActivatedOption(1);
+        this.option = InputOption.getInstance().getActivatedOption(InputOption.getInstance().OPTION2);
         this.emp = emp;
         this.source=source;
 
-        if(option== Option.NONE){
-            return matchAll();
-        }
-        return matchPart();
+        return (option== Option.NONE)? matchAll():matchPart();
     }
 
     private boolean matchAll() {
@@ -26,6 +23,13 @@ public class PhoneNumberSearch implements SearchPolicy {
     }
 
     private boolean matchPart(){
-        return((option==Option.MIDDLE_PHONENUMBER) && source.equals(emp.getPhoneMiddleNum())) ||  (option==Option.LAST_PHONENUMBER&& source.equals(emp.getPhoneEndNum()));
+        return matchMiddlePhoneNum()||matchLastPhoneNum();
+    }
+
+    private boolean matchMiddlePhoneNum(){
+        return (option==Option.MIDDLE_PHONENUMBER) && source.equals(emp.getPhoneMiddleNum());
+    }
+    private  boolean matchLastPhoneNum(){
+        return (option==Option.LAST_PHONENUMBER) && source.equals(emp.getPhoneEndNum());
     }
 }
